3f3fdaf1d1207320b74fa2906d0465cb73d029bf
Reera <126323642+FatemehTehranizadeh@users.noreply.github.com>
2025-05-11 13:09:52 +0330

    Some basic concepts of Go

---
+++ basics/variables.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+var job string = "engineer"
+
+func main() {
+	// var age int
+	// var firstName string = "Reera"
+	// var lastName string = "Tehranizadeh"
+	
+	// isStudent := false
+	isMarried := true
+
+	fmt.Println(isMarried)
+
+	const MAX = 6
+	fmt.Println(MAX)
+
+
+}
