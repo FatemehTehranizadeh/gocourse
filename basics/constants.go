2e19be3004723bf9e22fe05706e5aa35352bb91b
Reera <126323642+FatemehTehranizadeh@users.noreply.github.com>
2025-05-12 15:19:25 +0330

    constants in Go

---
+++ basics/constants.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+const pi = 3.14
+const GRAVITY = 9.81
+
+func main() {
+	const day int = 7
+
+	// const block
+	const (
+
+		monday = 1
+		tuesday = 2
+		wednesday int = 3
+	)
+	fmt.Println("Monday is the",monday,"day of the week")
+
+}
