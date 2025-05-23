3113e30125687f6ad9bd9ff370b21b892fb96abc
Reera <126323642+FatemehTehranizadeh@users.noreply.github.com>
2025-05-23 19:23:38 +0330

    Range function in Go and using it for iteration

---
+++ basics/range.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	//Range keyword uses for iterating over data structures like arrays
+	//Iterating over channels is common
+	//Iterating over strings
+
+	message := "Hello World"
+
+	for i, v := range message {
+		// fmt.Println("index: ", i, "Value: ", v) //Prints the unicode values
+		fmt.Printf("Index: %d, Rune: %c\n", i, v)
+	}
+	//Range works on a copy of the variable
+	//Range iterates over arrays and slices and strings in order from the first element to the last one
+	//For maps, it iterates over key-value pairs in a unspcified order
+	//For channels, it iterates until the channel is closed.
+
+
+}
